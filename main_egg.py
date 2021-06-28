0a602bac013c1a20d3104e68d9a2df2ce6d7c42f
Recep Avci <ravc486@bn337068.uoa.auckland.ac.nz>
2021-06-29 09:14:06 +1200

    update

--- main_egg.py
+++ main_egg.py
@@ -3,23 +3,25 @@ from ESINet.util import *
 from ESINet.ann import *
 
 ### Import source data
-def import_source()
+def import_source():
     # Write a function to import voxelised dipole information of all cases/simulations
     return sources_sim # np array of nSim x nVoxels x 1
 
-def import_simulated_data()
+def import_simulated_data():
     # Write a function to import simulation results of all cases/simulations
     return egg_sim # np array of nSim x nNodes x 1
 
-### Load and train ConvDip with simulated data
-# Initialize the artificial neural network model
-input_dim = ? #total number of voxels
-output_dim = ? #number of selected nodes
-model = get_model(input_dim, output_dim)
+if __name__ == '__main__':
+    ### Load and train ConvDip with simulated data
+    # Initialize the artificial neural network model
+    input_dim = ? #total number of voxels
+    output_dim = ? #number of selected nodes
+    model = get_model(input_dim, output_dim)
 
-# Train the model
-model, history = train_model(model, sources_sim, egg_sim, delta=1)
+    # Train the model
+    model, history = train_model(model, sources_sim, egg_sim, delta=1)
 
-### Evaluate ConvDip
-# Predict
-source_predicted = predict(model, egg_sample, pth_fwd)
+    ### Test the model
+    # Predict
+    source_predicted = predict(model, egg_sample, pth_fwd)
+    # Modify predict function not to use pth_fwd or leadfield.
